a2bafacc2d1846636f66bb2af33ea2b691295da8
Arcyvilk <blazemakerrr@gmail.com>
2023-12-21 00:24:33 +0100

    refactor: 💡 change attribution for the map layer

--- src/pages/HomeView/Map.tsx
+++ src/pages/HomeView/Map.tsx
@@ -37,7 +37,10 @@ export const Map = () => {
       scrollWheelZoom
       className={s.mapContainer}>
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>
+        &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a>
+        &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a>
+        &copy; <a href="https://www.openstreetmap.org/copyright/" target="_blank">OpenStreetMap contributors</a>'
         url="https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg"
       />
       <Marker position={L.latLng(coords[0], coords[1])}>
