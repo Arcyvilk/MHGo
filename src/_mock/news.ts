b9a1e7eb2b0163700ef71272eaee6825f2f36380
Arcyvilk <arcyvilk@gmail.com>
2023-12-20 13:35:55 +0100

    refactor: 💡 typo in news

--- src/_mock/news.ts
+++ src/_mock/news.ts
@@ -11,7 +11,7 @@ export const news: News[] = [
     date: new Date(1705392000000),
     author: 'Arcyvilk',
     title: 'Happy birthday!',
-    content: '% has birthday today. Hpapy birthday!',
+    content: '% has birthday today. Happy birthday!',
     img: 'https://cdn.arcyvilk.com/mhgo/happybday.jpg',
   },
 ];
