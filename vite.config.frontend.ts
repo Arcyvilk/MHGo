26aac8c7176395ff0934601e76815571b3b6d65e
Arcyvilk <arcyvilk@gmail.com>
2023-12-20 13:37:26 +0100

    feat: 🎸 register service workers

--- vite.config.frontend.ts
+++ vite.config.frontend.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
         // svgr options
       },
     }),
+    VitePWA({ registerType: 'autoUpdate' }),
   ],
 });
