d64b349ce99f052887df50f49b9905ec055609c7
Arcyvilk <arcyvilk@gmail.com>
2023-12-20 14:52:10 +0100

    refactor: 💡 viewport changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sesah9pi5pg"
+    "revision": "0.3a4dtm8ud0o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
