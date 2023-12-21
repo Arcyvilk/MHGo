dc61709411e132293a18824f52c16aaa29c72418
Arcyvilk <arcyvilk@gmail.com>
2023-12-21 16:39:43 +0100

    refactor: 💡 more drops from monsters

--- src/_mock/drops.ts
+++ src/_mock/drops.ts
@@ -45,8 +45,98 @@ export const monsterDrops: MonsterDrop[] = [
         level: 1,
         drops: [
           {
-            materialId: 'claw1',
-            chance: 100,
+            materialId: 'hide1',
+            chance: 50,
+            amount: 6,
+          },
+        ],
+      },
+      {
+        level: 2,
+        drops: [
+          {
+            materialId: 'hide1',
+            chance: 70,
+            amount: 6,
+          },
+          {
+            materialId: 'hide2',
+            chance: 30,
+            amount: 4,
+          },
+        ],
+      },
+      {
+        level: 3,
+        drops: [
+          {
+            materialId: 'hide1',
+            chance: 70,
+            amount: 6,
+          },
+          {
+            materialId: 'hide2',
+            chance: 70,
+            amount: 4,
+          },
+          {
+            materialId: 'hide3',
+            chance: 30,
+            amount: 3,
+          },
+        ],
+      },
+      {
+        level: 4,
+        drops: [
+          {
+            materialId: 'hide1',
+            chance: 70,
+            amount: 6,
+          },
+          {
+            materialId: 'hide2',
+            chance: 70,
+            amount: 4,
+          },
+          {
+            materialId: 'hide3',
+            chance: 70,
+            amount: 3,
+          },
+          {
+            materialId: 'hide4',
+            chance: 30,
+            amount: 2,
+          },
+        ],
+      },
+      {
+        level: 5,
+        drops: [
+          {
+            materialId: 'hide1',
+            chance: 70,
+            amount: 6,
+          },
+          {
+            materialId: 'hide2',
+            chance: 70,
+            amount: 4,
+          },
+          {
+            materialId: 'hide3',
+            chance: 70,
+            amount: 3,
+          },
+          {
+            materialId: 'hide4',
+            chance: 70,
+            amount: 2,
+          },
+          {
+            materialId: 'hide5',
+            chance: 30,
             amount: 1,
           },
         ],
@@ -60,8 +150,98 @@ export const monsterDrops: MonsterDrop[] = [
         level: 1,
         drops: [
           {
-            materialId: 'claw1',
-            chance: 100,
+            materialId: 'fin1',
+            chance: 50,
+            amount: 6,
+          },
+        ],
+      },
+      {
+        level: 2,
+        drops: [
+          {
+            materialId: 'fin1',
+            chance: 70,
+            amount: 6,
+          },
+          {
+            materialId: 'fin2',
+            chance: 30,
+            amount: 4,
+          },
+        ],
+      },
+      {
+        level: 3,
+        drops: [
+          {
+            materialId: 'fin1',
+            chance: 70,
+            amount: 6,
+          },
+          {
+            materialId: 'fin2',
+            chance: 70,
+            amount: 4,
+          },
+          {
+            materialId: 'fin3',
+            chance: 30,
+            amount: 3,
+          },
+        ],
+      },
+      {
+        level: 4,
+        drops: [
+          {
+            materialId: 'fin1',
+            chance: 70,
+            amount: 6,
+          },
+          {
+            materialId: 'fin2',
+            chance: 70,
+            amount: 4,
+          },
+          {
+            materialId: 'fin3',
+            chance: 70,
+            amount: 3,
+          },
+          {
+            materialId: 'fin4',
+            chance: 30,
+            amount: 2,
+          },
+        ],
+      },
+      {
+        level: 5,
+        drops: [
+          {
+            materialId: 'fin1',
+            chance: 70,
+            amount: 6,
+          },
+          {
+            materialId: 'fin2',
+            chance: 70,
+            amount: 4,
+          },
+          {
+            materialId: 'fin3',
+            chance: 70,
+            amount: 3,
+          },
+          {
+            materialId: 'fin4',
+            chance: 70,
+            amount: 2,
+          },
+          {
+            materialId: 'fin5',
+            chance: 30,
             amount: 1,
           },
         ],
@@ -75,8 +255,98 @@ export const monsterDrops: MonsterDrop[] = [
         level: 1,
         drops: [
           {
-            materialId: 'claw1',
-            chance: 100,
+            materialId: 'scale1',
+            chance: 50,
+            amount: 6,
+          },
+        ],
+      },
+      {
+        level: 2,
+        drops: [
+          {
+            materialId: 'scale1',
+            chance: 70,
+            amount: 6,
+          },
+          {
+            materialId: 'scale2',
+            chance: 30,
+            amount: 4,
+          },
+        ],
+      },
+      {
+        level: 3,
+        drops: [
+          {
+            materialId: 'scale1',
+            chance: 70,
+            amount: 6,
+          },
+          {
+            materialId: 'scale2',
+            chance: 70,
+            amount: 4,
+          },
+          {
+            materialId: 'scale3',
+            chance: 30,
+            amount: 3,
+          },
+        ],
+      },
+      {
+        level: 4,
+        drops: [
+          {
+            materialId: 'scale1',
+            chance: 70,
+            amount: 6,
+          },
+          {
+            materialId: 'scale2',
+            chance: 70,
+            amount: 4,
+          },
+          {
+            materialId: 'scale3',
+            chance: 70,
+            amount: 3,
+          },
+          {
+            materialId: 'scale4',
+            chance: 30,
+            amount: 2,
+          },
+        ],
+      },
+      {
+        level: 5,
+        drops: [
+          {
+            materialId: 'scale1',
+            chance: 70,
+            amount: 6,
+          },
+          {
+            materialId: 'scale2',
+            chance: 70,
+            amount: 4,
+          },
+          {
+            materialId: 'scale3',
+            chance: 70,
+            amount: 3,
+          },
+          {
+            materialId: 'scale4',
+            chance: 70,
+            amount: 2,
+          },
+          {
+            materialId: 'scale5',
+            chance: 30,
             amount: 1,
           },
         ],
