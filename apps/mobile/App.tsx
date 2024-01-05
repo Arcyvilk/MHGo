65812b28078f2297dc3f0d0f9cd7976f7946acd0
Arcyvilk <arcyvilk@gmail.com>
2024-01-05 11:13:22 +0100

    refactor: 💡 attempt to fix bottom bar in mobile

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,15 +1,33 @@
 import * as React from 'react';
 import { WebView } from 'react-native-webview';
-import { StyleSheet } from 'react-native';
+import { PermissionsAndroid, StyleSheet } from 'react-native';
 
-export default function App() {
+import * as NavigationBar from 'expo-navigation-bar';
+
+PermissionsAndroid.request(
+  PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+  {
+    title: 'Location Access Permission',
+    message:
+      'To be able to play Master Hoarder GO!, we need your permission to access your location. We will use it only for gameplay purposes.',
+    buttonPositive: 'Okay!',
+    buttonNegative: 'Not okay!',
+  },
+);
+
+const App = () => {
+  NavigationBar.setVisibilityAsync('hidden');
+
   return (
     <WebView
       style={styles.container}
       source={{ uri: 'https://app.masterhoarder.site' }}
+      geolocationEnabled={true}
     />
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
