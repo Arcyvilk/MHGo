4720e1fa115371afe78556af493c5ebc8330bda0
Arcyvilk <blazemakerrr@gmail.com>
2024-01-07 00:39:21 +0100

    fix: 🐛 nuke gif not restarting

--- packages/front/components/Animations/Nuke/Nuke.tsx
+++ packages/front/components/Animations/Nuke/Nuke.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { v4 as uuid } from 'uuid';
 
 import { SoundSE, useSounds } from '../../../hooks';
@@ -6,17 +6,16 @@ import { CDN_URL } from '../../../env';
 import s from './Nuke.module.scss';
 
 export const Nuke = () => {
+  // This is a hack to restart a non-looped gif on page rerender.
+  const [src, setSrc] = useState(`${CDN_URL}/misc/dummy.gif`);
   const { playSESound } = useSounds();
 
   useEffect(() => {
     playSESound(SoundSE.NUKE);
+    setTimeout(() => {
+      setSrc(`${CDN_URL}/misc/explosion.gif`);
+    }, 0);
   }, []);
 
-  return (
-    <img
-      key={uuid()}
-      className={s.nuke}
-      src={`${CDN_URL}/misc/explosion.gif`}
-    />
-  );
+  return <img key={uuid()} className={s.nuke} src={src} />;
 };
