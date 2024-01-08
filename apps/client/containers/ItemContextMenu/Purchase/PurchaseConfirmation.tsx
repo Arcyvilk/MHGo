8216c9cbf97e072e380e6bd7e6664098bf640144
Arcyvilk <arcyvilk@gmail.com>
2024-01-08 18:37:44 +0100

    feat: 🎸 shop is working now

--- apps/client/containers/ItemContextMenu/Purchase/PurchaseConfirmation.tsx
+++ apps/client/containers/ItemContextMenu/Purchase/PurchaseConfirmation.tsx
@@ -1,6 +1,19 @@
-import { Button } from '@mhgo/front';
+import { useEffect, useMemo } from 'react';
+import {
+  Button,
+  Icon,
+  IconType,
+  Size,
+  useItemsApi,
+  useSettingsApi,
+  useUserPurchaseItemApi,
+  useUserWealthApi,
+} from '@mhgo/front';
+import { Currency } from '@mhgo/types';
+import { useUser } from '../../../hooks/useUser';
 
 import s from './PurchaseConfirmation.module.scss';
+import { toast } from 'react-toastify';
 
 type PurchaseConfirmationProps = {
   itemId: string;
@@ -11,12 +24,8 @@ export const PurchaseConfirmation = ({
   itemId,
   setIsModalOpen,
 }: PurchaseConfirmationProps) => {
-  const { item, moneyNeeded, onPurchase, isSuccess } = useItemPurchase(itemId);
-
-  const canBePurchaseed = moneyNeeded.reduce((sum, curr) => {
-    if (curr.userAmount < curr.amount) return false;
-    return sum;
-  }, true);
+  const { item, moneyNeeded, onPurchase, canBePurchased } =
+    useItemPurchase(itemId);
 
   const onYes = () => {
     onPurchase();
@@ -30,12 +39,21 @@ export const PurchaseConfirmation = ({
       <h2 className={s.purchaseConfirmation__prompt}>
         Purchasing {item?.name ?? 'item'}...
       </h2>
-      <p className={s.purchaseConfirmation__text}>
-        Purchasing{' '}
-        <span style={{ fontWeight: 800 }}>{item?.name ?? 'this'}</span> will
-        consume the following materials:
-      </p>
-      <div className={s.purchaseConfirmation__materials}>ALL OF 'EM</div>
+      <p className={s.purchaseConfirmation__text}>This will cost you:</p>
+      <div className={s.purchaseConfirmation__lists}>
+        {moneyNeeded.map(money => {
+          const isTooPoor = money?.userAmount < money?.amount;
+          return (
+            <div className={s.purchaseConfirmation__price}>
+              <Icon icon={money.icon} size={Size.MICRO} />
+              <span style={isTooPoor ? { color: 'red' } : { color: 'green' }}>
+                {money?.userAmount}
+              </span>
+              /<span>{money?.amount}</span>
+            </div>
+          );
+        })}
+      </div>
       <div className={s.purchaseConfirmation__buttons}>
         <Button
           label="Cancel"
@@ -47,8 +65,8 @@ export const PurchaseConfirmation = ({
           label="Purchase"
           onClick={onYes}
           simple
-          disabled={!canBePurchaseed}
-          title={canBePurchaseed ? null : 'You are too poor for this!'}
+          disabled={!canBePurchased}
+          title={canBePurchased ? null : 'You are too poor for this!'}
         />
       </div>
     </div>
@@ -56,12 +74,56 @@ export const PurchaseConfirmation = ({
 };
 
 const useItemPurchase = (itemId: string) => {
-  // useUserPurchaseItemApi
+  const { userId } = useUser();
+  const { setting: currencies } = useSettingsApi<Currency[]>(
+    'currency_types',
+    [],
+  );
+  const { data: userWealth } = useUserWealthApi(userId);
+  const { data: items } = useItemsApi();
+  const {
+    mutate: mutateItemPurchase,
+    isSuccess,
+    isError,
+  } = useUserPurchaseItemApi(userId, itemId);
+
+  const item = useMemo(() => {
+    return items.find(i => i.id === itemId);
+  }, [itemId]);
+
+  const moneyNeeded = (item?.price ?? []).map(price => {
+    const userAmount =
+      userWealth.find(currency => currency.id === price.id)?.amount ?? 0;
+    const icon = (currencies?.find(currency => currency.id === price.id)
+      ?.icon ?? 'Question') as IconType;
+    return {
+      ...price,
+      icon,
+      userAmount,
+    };
+  });
+
+  const canBePurchased = !moneyNeeded?.some(
+    money => money.amount > money.userAmount,
+  );
+
+  const onPurchase = () => {
+    mutateItemPurchase({ amount: 1 });
+  };
+
+  useEffect(() => {
+    if (isSuccess) toast.success('Item successfully purchased!');
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (isError) toast.error('Crafting failed! Your money was not spent.');
+  }, [isError]);
+
   return {
-    item: { id: itemId, name: 'dupa' },
-    moneyNeeded: [{ type: 'base', amount: 1, userAmount: 0 }],
-    onPurchase: () => {},
-    getItemIngredients: () => {},
-    isSuccess: true,
+    item,
+    moneyNeeded,
+    canBePurchased,
+    isSuccess,
+    onPurchase,
   };
 };
