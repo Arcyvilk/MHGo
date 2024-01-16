7970428a69b98a1279fb7ebecc69549fe6ff314c
Arcyvilk <arcyvilk@gmail.com>
2024-01-16 11:12:05 +0100

    fix: 🐛 nuke not restarting on mobile

--- packages/front/components/Animations/Nuke/Nuke.tsx
+++ packages/front/components/Animations/Nuke/Nuke.tsx
@@ -7,12 +7,12 @@ import s from './Nuke.module.scss';
 
 export const Nuke = () => {
   // This is a hack to restart a non-looped gif on page rerender.
-  const [src, setSrc] = useState('');
+  const [src, setSrc] = useState(`${CDN_URL}/misc/dummy.gif`);
   const { playSound } = useSounds(undefined);
 
   useEffect(() => {
     setTimeout(() => {
-      setSrc(`${CDN_URL}/misc/explosion.gif`);
+      setSrc(`${CDN_URL}/misc/explosion.gif?test=${uuid()}`);
     }, 0);
     playSound(SoundSE.NUKE);
   }, []);
