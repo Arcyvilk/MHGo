3190e804fe559f3ffb08b33012ac720c1415a2df
Arcyvilk <arcyvilk@gmail.com>
2024-01-16 10:50:51 +0100

    test: 💍 another nuke test 2

--- packages/front/components/Animations/Nuke/Nuke.tsx
+++ packages/front/components/Animations/Nuke/Nuke.tsx
@@ -7,18 +7,19 @@ import s from './Nuke.module.scss';
 
 export const Nuke = () => {
   // This is a hack to restart a non-looped gif on page rerender.
-  const [src, setSrc] = useState(`${CDN_URL}/misc/dummy.gif`);
+  const [src, setSrc] = useState('');
   const { playSound } = useSounds(undefined);
 
   useEffect(() => {
     setTimeout(() => {
       setSrc(`${CDN_URL}/misc/explosion.gif`);
-    }, 1);
+    }, 0);
     playSound(SoundSE.NUKE);
   }, []);
 
   return (
     <>
+      <span style={{ position: 'absolute', top: '20px' }}>Im loases</span>
       <span style={{ position: 'absolute', top: 0 }}>{src}</span>
       <img key={uuid()} className={s.nuke} src={src} />
     </>
