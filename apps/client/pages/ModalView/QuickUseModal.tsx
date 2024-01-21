ae406c54b436c6085ce5bf2730997ccb218e5f37
Arcyvilk <blazemakerrr@gmail.com>
2024-01-21 21:22:51 +0100

    refactor: 💡 even more skeletons

--- apps/client/pages/ModalView/QuickUseModal.tsx
+++ apps/client/pages/ModalView/QuickUseModal.tsx
@@ -2,8 +2,23 @@ import { useUser, useUserItems } from '../../hooks/useUser';
 import { ItemContextMenu } from '../../containers';
 
 import s from './ModalView.module.scss';
+import { Item, QueryBoundary } from '@mhgo/front';
 
-export const QuickUseModal = () => {
+export const QuickUseModal = () => (
+  <QueryBoundary
+    fallback={
+      <div className={s.modalView__quickUse}>
+        <h2 className={s.modalView__quickUse__title}>Quick use menu</h2>
+        <div className={s.modalView__quickUse__items}>
+          {new Array(4).fill(<Item.Skeleton />)}
+        </div>
+      </div>
+    }>
+    <Load />
+  </QueryBoundary>
+);
+
+const Load = () => {
   const { consumableItems } = useConsumableItems();
   const hasQuickUseItems = consumableItems?.length > 0;
 
