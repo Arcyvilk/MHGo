29ba9dfe99911d0b1f6debfc0efa58f28aaaaea6
Arcyvilk <arcyvilk@gmail.com>
2024-02-14 12:46:29 +0100

    refactor: 💡 show active page on admins sidebar

--- apps/admin/containers/Sidebar/Sidebar.tsx
+++ apps/admin/containers/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Icon, Loader, QueryBoundary, Size } from '@mhgo/front';
+import { Icon, Loader, QueryBoundary, Size, modifiers } from '@mhgo/front';
 
 import { Entry, entries } from '../../utils/entries';
 import { useMe } from '../../utils/useMe';
@@ -17,6 +17,7 @@ export const Sidebar = (props: SidebarProps) => (
 const Load = ({ title }: SidebarProps) => {
   const { isLoggedIn, isAdmin, logoutUser } = useMe();
   const navigate = useNavigate();
+
   const onEntryClick = (entry: Entry) => {
     if (!isLoggedIn || !isAdmin) return;
     navigate(entry.link);
@@ -32,7 +33,9 @@ const Load = ({ title }: SidebarProps) => {
           .map(entry => (
             <button
               key={entry.id}
-              className={s.sidebar__entry}
+              className={modifiers(s, 'sidebar__entry', {
+                isActive: location.pathname.includes(entry.id),
+              })}
               onClick={() => onEntryClick(entry)}>
               {!isLoggedIn || !isAdmin ? (
                 <Icon icon="Spin" spin size={Size.MICRO} />
