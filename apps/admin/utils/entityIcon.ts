1fdbcadfa665e7352c14799f964cc3cf08c4cb9b
Arcyvilk <blazemakerrr@gmail.com>
2025-02-26 18:44:28 +0100

    refactor: 💡 make entity icons into separate enum

---
+++ apps/admin/utils/entityIcon.ts
@@ -0,0 +1,11 @@
+import { IconType } from '@mhgo/front';
+
+export const EntityIcon: Record<string, IconType> = {
+  ITEMS: 'Armory',
+  MATERIALS: 'ItemBox',
+  HABITATS: 'Tree',
+  MONSTERS: 'Monster',
+  RESOURCES: 'Star',
+  QUESTS: 'Quest',
+  USERS: 'Friends',
+};
