9443e5afbd675d82243f6067ab17aac5b55950de
Arcyvilk <blazemakerrr@gmail.com>
2025-02-26 14:38:53 +0100

    refactor: 💡 update the ChangeReview type

--- packages/types/lib/ChangeReview.ts
+++ packages/types/lib/ChangeReview.ts
@@ -1,14 +1,57 @@
-export type ChangeReview = {
-  date: Date;
-  changeType: 'delete';
-  entityType: 'item' | 'material' | 'resource' | 'monster' | 'habitat';
-  entityName: string;
-  entityId: string;
-  affectedEntities: ChangeAffectedEntity[];
-};
+export type EntityType =
+  | 'item'
+  | 'material'
+  | 'resource'
+  | 'monster'
+  | 'habitat'
+  | 'quest'
+  | 'questDaily';
 
-export type ChangeAffectedEntity = {
-  id: string;
-  type: string;
+export type ChangeType = 'delete' | 'update';
+
+export type ChangeReview = {
+  /**
+   * Date when the change was made
+   */
+  date: Date;
+  /**
+   * ID of the admin who made the change
+   */
+  adminId: string;
+  /**
+   * ID of the entity that was indirectly changed by changing other entity
+   */
+  affectedEntityId: string;
+  /**
+   * Type of the entity that was indirectly changed by changing other entity
+   */
+  affectedEntityType: EntityType;
+  /**
+   * ID of the entity that was changed
+   */
+  changedEntityId: string;
+  /**
+   * Type of the entity that was changed
+   */
+  changedEntityType: EntityType;
+  /**
+   * Name of the entity that was changed (used if original entity was deleted)
+   */
+  changedEntityName: string;
+  /**
+   * Type of change made to the entity
+   */
+  changeType: ChangeType;
+  /**
+   * Relation to the indirectly affected entity.
+   * For example, we delete an "item".
+   * Another "item" is affected by this deletion because it was its crafting ingridient.
+   * Therefore, "relationAffected" is "itemCraft" because that's the name of the collection
+   * where the deleted entity was referenced by the affected entity.
+   */
+  relation: string;
+  /**
+   * Flag if the change was already reviewed and approved or not.
+   */
   isApproved: boolean;
 };
